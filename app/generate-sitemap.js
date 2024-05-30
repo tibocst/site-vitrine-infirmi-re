39f69635bc50fa691a2037ebbddc3a54e6e1a67a
tibocst <thibault.coste.233@gmail.com>
2024-05-30 23:48:19 +0200

    del

--- app/generate-sitemap.js
+++
@@ -1,9 +0,0 @@
-const SitemapGenerator = require('sitemap-generator');
-
-// Créez un générateur
-const generator = SitemapGenerator('https://www.jardinparfait.com', {
-  stripQuerystring: false,
-});
-
-// Démarrez le générateur
-generator.start();
